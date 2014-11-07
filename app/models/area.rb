3ed68282a1bf9da5ca001424398d78e898ae30ba
Diego Llamas <diegollams@gmail.com>
2014-11-06 23:10:16 -0600

    panels

--- app/models/area.rb
+++ app/models/area.rb
@@ -2,6 +2,6 @@ class Area < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :description, :presence => true
 	belongs_to :user
-	has_many :users
-	has_many :surgeries
+	has_many :users,  dependent: :nullify
+	has_many :surgeries 
 end
