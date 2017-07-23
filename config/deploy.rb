272756f0bdea679d02a9ebf29cd51dbe8b52a811
Roberto Ruiz Velasco Martinez <rob@thebitcorps.com>
2017-07-23 16:26:26 -0500

    x

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,12 +12,12 @@ set :puma_threads, [4, 16]
 set :puma_workers, 0
 
 # Don't change these unless you know what you're doing
-set :use_sudo, false
+# set :use_sudo, false
 set :pty, true
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(/Users/rruizv/.ssh/id_rsa.pub) }
 # set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :stage, :production
-set :deploy_via, :copy
+# set :deploy_via, :copy
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
@@ -36,55 +36,6 @@ set :puma_init_active_record, true # Change to false when not using ActiveRecord
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_files, %w{.env.production}
-
-# namespace :puma do
-#   desc 'Create Directories for Puma Pids and Socket'
-#   task :make_dirs do
-#     on roles(:app) do
-#       execute "mkdir #{shared_path}/tmp/sockets -p"
-#       execute "mkdir #{shared_path}/tmp/pids -p"
-#     end
-#   end
-
-#   # before :starting, :make_dirs
-# end
-
-# namespace :deploy do
-#   desc "Make sure local git is in sync with remote."
-#   task :check_revision do
-#     on roles(:app) do
-#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#         puts "WARNING: HEAD is not the same as origin/master"
-#         puts "Run `git push` to sync changes."
-#         exit
-#       end
-#     end
-#   end
-
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
-
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # invoke 'puma:restart'
-#     end
-#   end
-
-#   before :starting, :check_revision
-#   after :finishing, :compile_assets
-#   after :finishing, :cleanup
-#   after :finishing, :restart
-# end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+# append :linked_files, %w{config/database.yml}
+# append :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+append :linked_files, %w{.env.production}
